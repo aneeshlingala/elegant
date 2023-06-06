0ae659e7a8f6c211d61df1f5ba678f9647b60c01
Aneesh Lingala <87113916+aneeshlingala@users.noreply.github.com>
2023-06-05 17:36:05 -0700

    added reinstall feature - and also upcoming feature, info

--- elegant.sh
+++ elegant.sh
@@ -30,6 +30,7 @@ if [ "$1" == "--help" ]; then
     echo "--remove : remove a package"
     echo "--help : see available options"
     echo "--reinstall : reinstall a package"
+    echo "--info : view information about a package"
     echo ""
     echo "That's it!"
 fi
@@ -43,7 +44,7 @@ fi
     if grep "$2" /usr/share/elegant/installed > /dev/null
     then
         echo "Error: $2 already installed... use --reinstall to reinstall."
-
+        exit
     else
         echo "Package $2 not installed, continuing..."
 fi
@@ -65,9 +66,31 @@ fi
 
     else
         echo "Error: $2 is not installed."
+        exit
 fi
 
     echo "Removing $2..."
     curl "https://raw.githubusercontent.com/aneeshlingala/elegant-pkgs/main/$2/pkgremove" | bash
     cat /usr/share/elegant/installed | sed "/$2/d"
 fi
+
+if [ "$1" == "--reinstall" ]; then
+     if grep "$2" == ""
+    then
+        echo "Error: no package specified."
+        exit
+fi
+    if grep "$2" /usr/share/elegant/installed > /dev/null
+    then
+        echo "$2 is installed... continuing."
+        
+    else
+        echo "Error: package $2 not installed."
+        exit
+fi
+
+    echo "Reinstalling $2..."
+    curl "https://raw.githubusercontent.com/aneeshlingala/elegant-pkgs/main/$2/pkgremove" | bash
+    curl "https://raw.githubusercontent.com/aneeshlingala/elegant-pkgs/main/$2/pkginstall" | bash
+
+    
